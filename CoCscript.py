add5df241a872832a24a06b0b6e0c52cbc4d4454
Matteo Cancian <cancian2003@gmail.com>
2024-10-04 13:50:43 +0200

    added CWL management, minor fixes and improvements

--- CoCscript.py
+++ CoCscript.py
@@ -8,23 +8,23 @@ import gspread
 from google.oauth2.service_account import Credentials
 
 
-# Function to make an HTTP request to the API
-def fetch_members_data_from_api(api_url, headers):
-    response = requests.get(api_url+"/members", headers)
+# Functions to make an HTTP request to the API
+def fetch_members_data_from_api(api_clan_url, headers):
+    response = requests.get(api_clan_url+"/members", headers)
     if response.status_code == 200:
         return response.json()
     # Error 5**: server side error
     elif response.status_code/100 == 5: 
         print("Il server di CoC ha qualche problema, riprovo tra 5 min")
         sleep(300) # sleep about 5 mins
-        fetch_members_data_from_api(api_url, headers)
+        fetch_members_data_from_api(api_clan_url, headers)
     # Other error
     else:
         raise Exception(f"Failed to fetch data from API. Status code: {response.status_code}. Response: {response.text}")
     
 # ---------------------------------------------------------------------------------------------------------------------------------
-def fetch_war_data_from_api(api_url, headers):
-    response = requests.get(api_url+"/currentwar", headers)
+def fetch_war_data_from_api(api_clan_url, headers):
+    response = requests.get(api_clan_url+"/currentwar", headers)
     # Response Ok
     if response.status_code == 200:
         api_data =  response.json()
@@ -35,18 +35,68 @@ def fetch_war_data_from_api(api_url, headers):
             secs_to_war_end = (datetime.strptime(api_data['endTime'], date_format) - datetime.now()).total_seconds()
             print("War in corso, dormo fino alla fine")
             sleep(secs_to_war_end+30) # sleep until war is ended
-            check_clan_members(api_url, headers)
-            fetch_war_data_from_api(api_url, headers)
+            check_clan_members(api_clan_url, headers)
+            fetch_war_data_from_api(api_clan_url, headers)
         elif api_data['state']=="notInWar":
             return None
     # Error 5**: server side error
     elif response.status_code/100 == 5: 
         print("Il server di CoC ha qualche problema, riprovo tra 5 min")
         sleep(300) # sleep about 5 mins
-        fetch_war_data_from_api(api_url, headers)
+        fetch_war_data_from_api(api_clan_url, headers)
     # Other error
     else:
         raise Exception(f"Failed to fetch data from API. Status code: {response.status_code}. Response: {response.text}")
+
+# ---------------------------------------------------------------------------------------------------------------------------------
+def fetch_warleague_wartags_data_from_api(api_url, path_clan_info, headers):
+    response = requests.get(api_url+path_clan_info+"/currentwar/leaguegroup", headers)
+    if response.status_code == 200:
+        api_data =  response.json()
+        if api_data['state']=="warEnded":   #war ended, we can fetch the results
+            war_tags = api_data["rounds"]["warTags"]
+            CWL_rounds = []
+            for war_tag in war_tags:
+                CWL_rounds.append(fetch_warleague_results_from_api(api_url+"/clanwarleagues/wars/", war_tag, headers))
+            return CWL_rounds   # IDEAL END
+        elif(api_data['state']=="preparation" or api_data['state']=="inWar"): #war ongoing, we need to wait
+            date_format = "%Y%m%dT%H%M%S.%fZ"
+            secs_to_war_end = (datetime.now().replace(day=10) - datetime.now()).total_seconds()
+            print("War in corso, dormo fino alla fine")
+            sleep(secs_to_war_end+300) # sleep until war is ended
+            requests.get(api_url+path_clan_info+"/currentwar/leaguegroup", headers)
+            api_data =  response.json()
+            if(api_data['state']=="preparation" or api_data['state']=="inWar"):
+                last_war_tag = api_data["rounds"]["warTags"][-1]
+                response = requests.get(api_url+"/clanwarleagues/wars/"+last_war_tag, headers)
+                api_data =  response.json()
+                secs_to_war_end = (datetime.strptime(api_data['endTime'], date_format) - datetime.now()).total_seconds()
+                print("Ultima war in corso, dormo fino alla fine")
+                sleep(secs_to_war_end+30) # sleep until war is ended
+            check_clan_members(api_url+path_clan_info, headers)
+            fetch_warleague_wartags_data_from_api(api_url, path_clan_info, headers)
+
+    elif response.status_code == 404: 
+        print("Non ci sono Clan War League in corso, controllo la war normale")
+        return None
+    # Error 5**: server side error
+    elif response.status_code/100 == 5: 
+        print("Il server di CoC ha qualche problema, riprovo tra 5 min")
+        sleep(300) # sleep 5 mins
+        fetch_warleague_wartags_data_from_api(api_url, path_clan_info, headers)
+    # Other error
+    else:
+        raise Exception(f"Failed to fetch data from API. Status code: {response.status_code}. Response: {response.text}")
+
+def fetch_warleague_results_from_api(api_wars_url, war_tag, headers):
+    response = requests.get(api_wars_url+war_tag, headers)
+    if response.status_code == 200:
+        api_data =  response.json()
+        return api_data
+    elif response.status_code/100 == 5: 
+        print("Il server di CoC ha qualche problema, riprovo tra 5 min")
+        sleep(300) # sleep 5 mins
+        fetch_warleague_results_from_api(api_url+"/clanwarleagues/wars/", war_tag, headers)
 
 
 # Functions to manipulate the JSON data as needed
@@ -66,10 +116,45 @@ def sumAttacksStarsOfAMember(member, max_attacks=2):
     else:
         return "="+str(sum)+"/"+str(max_stars)   # return the number of stars divided by the maximum (6)
 
-def manipulate_data_current_war(json_data): 
-    manipulated_data = [(member['tag'][1:], member['name'], sumAttacksStarsOfAMember(member, 2)) for member in (json_data['clan'])['members']]
-    return manipulated_data 
-    # Example: [('123456', 0.5), ('654321', 1), ('987654', 0)]
+def manipulate_data_current_war(json_data):
+    members_stars = {}
+    for member in json_data['clan']['members']:
+        tag = member['tag'][1:]
+        members_stars[tag] = 0
+        try:   #try if the member has attacked
+            members_stars[tag] += member["attacks"][0]["stars"]
+            members_stars[tag] += member["attacks"][1]["stars"]
+        except KeyError:    # no attacks -> no stars
+            pass
+    for member in members_stars:
+        members_stars[member] = "="+str(members_stars[member])+"/"+str(2*3)
+    return members_stars
+    # Example: {'123456': "=3/6", '654321': "=6/6", '987654': "=0/6"}
+# ---------------------------------------------------------------------------------------------------------------------------------
+def manipulate_data_cwl_rounds(rounds_data):
+    members_stars = {}
+    members_attacks_doable = {}
+    members_attacks_done = {}
+    for round in rounds_data:
+        for member in round['clan']['members']:
+            tag = member['tag'][1:]
+            if members_attacks_doable.get(tag, None) == None:
+                members_stars[tag] = 0
+                members_attacks_done[tag] = 0
+                members_attacks_doable[tag] = 0
+            try:   #try if the member has attacked
+                stars = member["attacks"][0]["stars"]
+            except KeyError:    # no attacks -> no stars
+                stars = -1
+            if stars != -1:
+                members_stars[tag] += stars
+                members_attacks_done[tag] += 1
+            members_attacks_doable[tag] += 1
+    for member in members_attacks_doable:
+        members_stars[member] = "="+str(members_stars[member])+"/"+str(members_attacks_doable[member]*3)
+        members_attacks_done[member] = "="+str(members_attacks_done[member])+"/"+str(members_attacks_doable[member])
+    return members_stars, members_attacks_done
+    # Example: {'123456': "=14/21", '654321': "=18/21", '987654': "=11/15"}, {'123456': "=6/7", '654321': "=7/7", '987654': "=4/5"}
 
 
 # Functions to upload data to Google Sheet
@@ -164,6 +249,14 @@ def upload_data_to_google_sheet(sheet_key, credentials_path, sheet_n:int, data):
         sheet.update_cell(row, col, member[2])
 
 
+def upload_war_data_to_google_sheet(sheet_key, credentials_path, data):
+    upload_data_to_google_sheet(sheet_key, credentials_path, 0, data)
+
+
+def upload_cwl_data_to_google_sheet(sheet_key, credentials_path, manipulated_stars_data, manipulated_partecipation_data):
+    upload_data_to_google_sheet(sheet_key, credentials_path, 1, manipulated_stars_data)
+    upload_data_to_google_sheet(sheet_key, credentials_path, 2, manipulated_partecipation_data)
+
 # ___________________________________________________________________________________________________________________________________
 def check_clan_members(coc_api_url, coc_api_headers):
     api_data = fetch_members_data_from_api(coc_api_url, coc_api_headers)
@@ -187,16 +280,29 @@ sheet_key = os.environ.get('SHEET_KEY')
 credentials_path = os.environ.get('GOOGLE_CREDENTIALS_PATH')
 while(True):
     check_clan_members(api_url+path_clan_info, request_headers)
-    api_data = fetch_war_data_from_api(api_url+path_clan_info, request_headers)
+    api_data = fetch_warleague_wartags_data_from_api(api_url, path_clan_info, request_headers)
     if api_data is not None:
         print("API data: ")
         print(api_data)
-        # Manipulate the data: take the JSON and give an array of [(playerID: nOfStars)]
-        manipulated_data = manipulate_data_current_war(api_data)
+        # Manipulate the data: take the JSON and give two associative array: {playerID: nStars/TOT}, {playerID: nAttacks/TOT}
+        manipulated_stars_data, manipulated_partecipation_data = manipulate_data_cwl_rounds(api_data)
         print("\n\nDati manipolati: ")
-        print(manipulated_data)
-        # Upload data to Google Sheet
-        upload_data_to_google_sheet(sheet_key, credentials_path, 0, manipulated_data)
+        print(manipulated_stars_data, manipulated_partecipation_data)
+        # Upload cwl data to Google Sheet
+        upload_cwl_data_to_google_sheet(sheet_key, credentials_path, manipulated_stars_data, manipulated_partecipation_data)
+    else:
+        api_data = fetch_war_data_from_api(api_url+path_clan_info, request_headers)
+        if api_data is not None:
+            print("API data: ")
+            print(api_data)
+            # Manipulate the data: take the JSON and give an array of [(playerID, nStars)]
+            manipulated_data = manipulate_data_current_war(api_data)
+            print("\n\nDati manipolati: ")
+            print(manipulated_data)
+            # Upload war data to Google Sheet
+            upload_war_data_to_google_sheet(sheet_key, credentials_path, 0, manipulated_data)
+        else:
+            print("Il server non mi ha restituito nessun dato")
 
     print("Nessuna war in corso, dormo 10h")
     sleep(36000) # sleep about 10h
